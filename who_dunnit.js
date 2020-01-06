79604222b257e11c42526ba4336392b3eb3ce3b4
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-06 17:29:03 +0000

    oops - late first commit

---
+++ who_dunnit.js
@@ -0,0 +1,126 @@
+// episode 1
+//
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// predicted output = "Miss Scarlet"
+// reason = all declared as consts but all within the global function/block
+
+// episode 2
+
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// predicted output = error as trying to reassign to constant
+// reason = as above
+
+// episode 3
+
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// predicted output = First Verdict: The murderer is Mrs Peacock. Second Verdict: The murderer is Professor Plum.
+// reason = second let murderer variable only exists within declareMurderer function/block, won't change the first murderer variable.
+
+// episode 4
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// predicted output = The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+//                    Suspect three is Mrs. Peacock.
+// reason = let suspectThree = 'Colonel Mustard' only exists within the declareAllSuspects function/block.
+
+// episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// predicted output = The weapon is the Revolver.
+// reason = Although scenario is declared as a const, can still access and change it's properties.
+
+// episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// predicted output = The murderer is Mrs. White.
+// reason = let murderer is declared in the global function so is accessible within changeMurderer.
+
+// episode 7
+
+// predicted output =
+// reason =
